962e0899d042700889959bf86a0330305f95b35f
amondi614 <amondiocholla12@gmail.com>
2022-03-11 20:25:00 +0300

    final commit

--- script.js
+++ script.js
@@ -1,28 +1,74 @@
-const testWrapper = document.querySelector(".test-wrapper");
-const testArea = document.querySelector(".test-area");
-const originText = document.querySelector(".origin-text p");
-// .innerHTML;
-const resetButton = document.querySelector(".reset");
+const testWrapper = document.querySelector(".test-Wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
+var timer = [0, 0, 0, 0];
+var interval;
+//Add a leading zero to nubers 9 or below(purely for aesthetics)
+function leadingZero(time) {
+    if (time <= 9) {
+        time = "0" + time;
+    }
+    return time;
+}
+
+//Run a standard minute/second/hundreths timer:
+function runTimer() {
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    theTimer.innerHTML = currentTime;
+    timer[3]++;
+
+    timer[0] = Math.floor((timer[3] / 100) / 60);
+    timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
+    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+
+}
 
 //match the text entered with the provided test on the page
 function spellCheck() {
     let textEntered = testArea.value;
+    let originTextMatch = originText.substring(0, textEnteredLength);
+
+    clearInterval(interval);
+
+    if (textEntered == originText) {
+        testWrapper.style.borderColor = "#429890";
+    } else {
+        if (textEntered == originTextMatch) {
+            testWrapper.style.borderColor = "#65CCf3";
+        } else {
+            testWrapper.style.borderColor = "#E95D0F";
+        }
+    }
     console.log(textEntered);
 }
 //start timer
 function start() {
-    let textEnteredlength = testArea.value.length;
-    console.log(textEnteredlength);
+    let textEnteredLength = testArea.value.length;
+    if (textEnteredLength === 0 && !timerRunning) {
+        timerRunning = true;
+        setInterval(runTimer, 10);
+        interval = seInterval(runTimer, 10);
+    }
+    console.log(textEnteredLength);
 }
 //run timer
 
 //reset everything
 function reset() {
-    console.log("reset button has been pressed");
+    clearInterval(interval);
+    interval = null;
+    timer = [0, 0, 0, 0];
+    timerRunning = false;
+
+    testArea.value - "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey;"
+
 }
 
 //event listeners for keyboard input and the reset
 testArea.addEventListener("keypress", start, false);
 testArea.addEventListener("keyup", spellCheck, false);
-resetButton.addEventListener("click", reset, false)
+resetButton.addEventListener("click", reset, false);
